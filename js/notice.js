5191dd44c0742869213c7c0659178591fc28c775
Facundo Sanchez <facuujos@gmail.com>
2023-07-01 20:31:11 -0300

    spinner y todo el menu

--- js/notice.js
+++ js/notice.js
@@ -1,9 +1,12 @@
 import { getNotices } from "../classes/notice.js";
 import { generateNotice } from "../utils/notice.js";
+import { showSpinner } from "../utils/spinner.js";
+
+
 
 window.addEventListener('DOMContentLoaded', async () => {
   const container = document.querySelector('.noticias-container');
-
+  showSpinner(true);
   const data = await getNotices() || [];
   if (data.length > 0) {
     const str = generateNotice(data)
@@ -12,4 +15,5 @@ window.addEventListener('DOMContentLoaded', async () => {
     // mensaje de vacio
   }
 
+  showSpinner(false);
 })
